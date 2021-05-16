9b9ee3f1071c9f3cbf9ba9e281fa8d3e1bd6811d
Malcolm Yeh <malcolmyeh@gmail.com>
2021-05-15 23:08:56 -0400

    test

--- src/containers/Categories/Category.js
+++ src/containers/Categories/Category.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Container} from "react-bootstrap";
-import Fade from 'react-reveal/Fade';
+import Articles from "../Articles";
+
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default function Category() {
-  return (
-    <Container>
-      <Fade>
-        <h1>CATEGORY</h1>
-        <h3>UNDER CONSTRUCTION</h3>
-      </Fade>
-    </Container>
-  );
+  const { id } = useParams();
+  return Articles({ category: id });
 }
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Container} from "react-bootstrap";
-import Fade from 'react-reveal/Fade';
+import React from "react";
+// import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Fade from "react-reveal/Fade";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
@@ -11,7 +11,6 @@ export default function Search() {
       <Fade>
         <h1>SEARCH</h1>
         <h3>UNDER CONSTRUCTION</h3>
-
       </Fade>
     </Container>
   );
