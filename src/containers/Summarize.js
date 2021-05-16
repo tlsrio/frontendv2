45bcfc8b272a2081b3926da826957dd2173cc2f3
Zayaan Moez <zayaanmoez@cmail.carleton.ca>
2021-05-15 23:57:19 -0400

    Summarize features: QA, summary and sentiment

--- src/containers/Summarize.js
+++ src/containers/Summarize.js
@@ -11,25 +11,45 @@ import {
   Modal,
   Button,
   Grid,
+  Tabs, 
+  Tab,
+  Nav
 } from "react-bootstrap";
 import LoadingButton from "../components/LoadingButton";
 import axios from "axios";
+import validator from "validator"
 axios.defaults.withCredentials = true;
 
 export default function Summarize() {
   const [isLoadingSummary, setIsLoadingSummary] = useState(false);
-  const [isLoadingAnswer, setIsLoadingAnswer] = useState(true);
+  const [isLoadingAnswer, setIsLoadingAnswer] = useState(false);
+  const [articleText, setText] = useState("")
+  const [inputType, setInputType] = useState("link")
   const [summary, setSummary] = useState({text: "", reduced_by: 0});
+  const [sentiment, setSentiment] = useState({label: "", score: 0});
+  const [answer, setAnswer] = useState({answer: "", score: 0});
   const { isAuthenticated, userId } = useAppContext();
   // console.log("userId:", userId);
 
   // Form fields
   var [fields, handleFieldChange] = useFields({
     text: "",
-  }); // TODO: add question
+    link: "",
+    question: "",
+  });
+
+  function changeInputType(key) {
+    if (key === "link")
+      setInputType("link");
+    else
+      setInputType("text");
+  }
 
   function validateTextForm() {
-    return fields.text.length > 0;
+    if (inputType === "link")
+      return validator.isURL(fields.link);
+    else
+      return fields.text.length > 0;
   }
 
   function handleSubmitTextForm(event) {
@@ -40,57 +60,194 @@ export default function Summarize() {
   async function handleSubmitText(){
     setIsLoadingSummary(true);
     const article = {
-      text: fields.text
+      text: "",
     }
+
+    if(inputType === "link") {
+      try {
+        const res = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/api/urlToText`,
+          {link: fields.link}
+        )
+        setText(res.data.text)
+        article.text = res.data.text;
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setText(fields.text);
+      article.text = fields.text;
+    }
+
     try{
       const res = await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/api/summarize`,
         article
       );
-      console.log(res.data);
-      setSummary({text: res.data.text, reduced_by: res.data.reduced_by});
+      setSummary({text: res.data.summary, reduced_by: res.data.reducedBy});
+
+      const resSen = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/sentiment`,
+        { text: res.data.summary }
+      );
+      
+      setSentiment({label: resSen.data.label, score: resSen.data.score})
     } catch (e) {
       console.log(e);
     }
-    fields.text = "";
+    setAnswer({answer: "", score: 0});
     setIsLoadingSummary(false);
   }
-  // TODO: should we combine these are determine if input is text or link??
-  //   function renderArticleLinkForm() {}
-  function renderArticleTextForm() {
-    return (
-      <Form onSubmit={handleSubmitTextForm}>
-        <Form.Group>
-          <Form.Control
-            id="text"
-            value={fields.text}
-            as="textarea"
-            rows={15}
-            onChange={handleFieldChange}
-            style={{ width: "100%" }}
-          />
-        </Form.Group>
 
-        <Row className="justify-content-md-center">
-          <LoadingButton
-            type="submit"
-            isLoading={isLoadingSummary}
-            disabled={!validateTextForm()}
-          >
+  function handleSubmitQuestionForm(event) {
+    event.preventDefault();
+    handleSubmitQuestion()
+  }
+
+  async function handleSubmitQuestion(){
+    setIsLoadingAnswer(true);
+    const qa = {
+      question: fields.question,
+      text: articleText,
+    }
+
+    try{
+      const res = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/QA`,
+        qa
+      );
+      setAnswer({answer: res.data.answer, score: res.data.score});
+
+    } catch (e) {
+      console.log(e);
+    }
+    setIsLoadingAnswer(false);
+  }
+
+
+  function renderArticleForm() {
+    return (
+
+      <Form onSubmit={handleSubmitTextForm}>
+        <Tab.Container defaultActiveKey="link" onSelect={changeInputType} id="text-tab">
+          <Col>
+            <Row className="justify-content-center">
+              <Nav variant="tabs" className="flex-row" style={{fontSize: "30px"}}>
+                <Nav.Item>
+                  <Nav.Link eventKey="link">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="text">Text</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Row>
+
+            <Row className="justify-content-center" style={{marginTop: "3vh"}}>
+              <Tab.Content style={{ width: "90%" }}>
+                <Tab.Pane eventKey="link">
+                  <h5>Enter url for the page to summarize:</h5>
+                  <Form.Control
+                    id="link"
+                    value={fields.link}
+                    onChange={handleFieldChange}
+                  />
+                </Tab.Pane>
+                <Tab.Pane eventKey="text">
+                  <h5>Enter text to summarize:</h5>
+                  <Form.Control
+                    id="text"
+                    value={fields.text}
+                    as="textarea"
+                    rows={15}
+                    onChange={handleFieldChange}
+                  />
+                </Tab.Pane>
+              </Tab.Content>
+            </Row>
+          </Col>
+        </Tab.Container>
+
+        <Row className="justify-content-md-center" style={{marginTop: "5vh"}}>
+          <LoadingButton type="submit" isLoading={isLoadingSummary}
+            disabled={!validateTextForm()}>
             Summarize
           </LoadingButton>
         </Row>
+
       </Form>
     );
   }
 
   function renderSummary() {
     return(
+      <Tab.Container defaultActiveKey="summary" onSelect={changeInputType} id="text-tab">
+          <Col style={{marginTop: "6vh"}}>
+            <Row className="justify-content-center">
+              <Nav variant="tabs" className="flex-row" style={{fontSize: "20px"}}>
+                <Nav.Item>
+                  <Nav.Link eventKey="summary">Summary</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="sentiment">Sentiment</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="qa">Questions</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Row>
+
+            <Row className="justify-content-center" style={{marginTop: "3vh"}}>
+              <Tab.Content style={{ width: "90%" }}>
+                <Tab.Pane eventKey="summary">
+                  <div>
+                    {summary.text}
+                    <br/> <br/>
+                    {`Original text reduced by ${summary.reduced_by}%.`}
+                  </div>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="sentiment">
+                  <h4>Sentiment Analysis:</h4>
+                  <br/>
+                  {sentiment.label}
+                  <br/>
+                  Score: {sentiment.score}
+
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="qa">
+                  <h4>Extractive Question Answering:</h4>
+                  <br/>
+                  <Form onSubmit={handleSubmitQuestionForm}>
+                    <Row className="flex-row"> 
+                      <Col> <h5>Ask a question:</h5> </Col>
+                      <Col xs={9}> <Form.Control id="question" value={fields.question}
+                        onChange={handleFieldChange}/> </Col>
+                    </Row>
+                    <LoadingButton type="submit" isLoading={isLoadingAnswer}
+                      disabled={fields.question.length == 0} style={{marginTop: "2vh"}}>
+                      Ask
+                    </LoadingButton>
+                  </Form>
+                  <div style={{marginTop: "3vh"}}>
+                    {(isLoadingAnswer || answer.answer.length == 0 ? <></> : 
+                    <Fade> <p>{answer.answer}</p> <p>Score: {answer.score}</p></Fade>)}
+                  </div>
+                </Tab.Pane>
+                
+              </Tab.Content>
+            </Row>
+
+          </Col>
+      </Tab.Container>
+
+      /*9
       <div>
         {summary.text}
         <br/>
         {`Original text reduced by ${summary.reduced_by}%.`}
       </div>
+      */
     )
   }
 
@@ -106,7 +263,7 @@ export default function Summarize() {
         <br />
         <br />
 
-        {renderArticleTextForm()}
+        {renderArticleForm()}
         {(isLoadingSummary || summary.text.length == 0) ? <></> : <Fade>{renderSummary()}</Fade>}
       </Fade>
     </Container>
